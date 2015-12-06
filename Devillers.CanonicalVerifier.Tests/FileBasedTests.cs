1731b245022bf978372278e71a538b7c737b4cef
Martin Devillers <mma.devillers@gmail.com>
2015-12-06 13:23:04 +0100

    ADD: Tests

---
+++ Devillers.CanonicalVerifier.Tests/FileBasedTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using Devillers.CanonicalVerifier.Rules;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Devillers.CanonicalVerifier.Tests
+{
+    [TestClass]
+    public class FileBasedTests
+    {
+        [TestMethod]
+        [DeploymentItem("TestSet1")]
+        public void Given_TestSet1_Should_BeValid()
+        {
+            var validator = new CanonicalSchemaSetValidator();
+
+            var schemaSet = new CanonicalSchemaSet(".");
+
+            var result = validator.Validate(schemaSet);
+
+            Assert.IsTrue(result.IsValid, string.Join("; ", result.Errors.Select(x => x.ErrorMessage)));
+        }
+    }
+}
